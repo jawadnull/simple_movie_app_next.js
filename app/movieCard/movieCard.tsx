75fb2ad4dd9d04725c8b576815a89ed689b40478
jawad kadem <126715476+jawadnull@users.noreply.github.com>
2023-10-07 13:19:38 +0300

    first commit

---
+++ app/movieCard/movieCard.tsx
@@ -0,0 +1,23 @@
+
+
+const movieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }:any) => {
+
+    return (
+      <div className="movie" key={imdbID}>
+        <div>
+          <p>{Year}</p>
+        </div>
+  
+        <div>
+          <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
+        </div>
+  
+        <div>
+          <span>{Type}</span>
+          <h3>{Title}</h3>
+        </div>
+      </div>
+    );
+  }
+
+export default movieCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,89 @@
+
+'use client';
+
+import { useEffect } from 'react';
+import { useState } from 'react';
+import './globals.css';
+import MovieCard from './movieCard/movieCard'
 import Image from 'next/image'
-import styles from './page.module.css'
 
-export default function Home() {
+
+
+//Here is The movies Api key: af49ef9a
+const API_URL= 'http://www.omdbapi.com?apikey=af49ef9a';
+
+
+
+
+
+
+
+ function Home() {
+
+
+  // fetch The API to search for The movie title and setting the parameter (title) to string
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("007");
+  }, []);
+
+  const searchMovies = async (title:string) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+   <div className='app'>
+    <h1>Cinema Corn</h1>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
+    <div className='search'>
+      <input 
+       
+      placeholder='search for a movie' 
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      
+      <Image
+          src="/search.svg"
+          alt="search"
           width={180}
           height={37}
+          onClick={() => searchMovies(searchTerm)}
           priority
         />
-      </div>
+         
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+     </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+    {/* cheack if there is a movie or not and runder it if there is*/} 
+    
+    {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+   </div>
+    
+  );
 }
+
+//
+// export defult Home component
+//
+export default Home;
+
